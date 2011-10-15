94d60daad9fc821573017350d9738abcb7b6cbf5
Sean Ryan <sryan@evercrack.com>
2011-10-15 11:27:21 -0400

    Correct FSF address throughout files

--- TestServer/testserver.c
+++ TestServer/testserver.c
@@ -1,7 +1,7 @@
 /*
-    Generic Game Metaserver Test Server
+    Generic Game Metaserver Test Client
 
-    Copyright (C) 2000 Dragon Master
+    Copyright (C) 2000-2004 Dragon Master
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2 as
@@ -17,11 +17,11 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+    02110-1301 USA
 
-    The author can be reached via e-mail to dragonm@leech.org
+    The author can be reached via e-mail to dragonm@hypercubepc.com
 */
-
 #include "wrap.h"
 #include "dgclibig.h"
 
--- src/handshakemsg.cpp
+++ src/handshakemsg.cpp
@@ -1,7 +1,7 @@
 /*
-    Generic Game Metaserver
+    Generic Game Metaserver Test Client
 
-    Copyright (C) 2000-2001 Dragon Master
+    Copyright (C) 2000-2004 Dragon Master
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2 as
@@ -17,9 +17,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+    02110-1301 USA
 
-    The author can be reached via e-mail to dragonm@leech.org
+    The author can be reached via e-mail to dragonm@hypercubepc.com
 */
 #include "handshakemsg.hh"
 
